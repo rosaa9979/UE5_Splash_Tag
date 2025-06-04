661a93494b79b694cd6d40cbea8bfc4188ddcd06
송준석 <rosaa9979@gmail.com>
2025-06-04 20:54:30 +0900

    gameinstance의 session부분을 subsystem으로 기능 분리

--- Source/TeamProject/Public/Network/MainGameInstance.h
+++ Source/TeamProject/Public/Network/MainGameInstance.h
@@ -4,9 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
-#include "Logging/LogMacros.h"
-#include "Interfaces/OnlineSessionInterface.h"
 #include "MainGameInstance.generated.h"
+
+class USteamSessionSubsystem;
 
 /**
  * 
@@ -17,39 +17,15 @@ class TEAMPROJECT_API UMainGameInstance : public UGameInstance
 	GENERATED_BODY()
 	
 public:
-	UMainGameInstance();
+	virtual void Init() override;
+
+private:
+	USteamSessionSubsystem* SessionManager;
 
 public:
+	UFUNCTION(BlueprintCallable)
 	void CreateGameSession();
+
+	UFUNCTION(BlueprintCallable)
 	void JoinGameSession();
-
-private:
-	//Session생성 성공시 Delegate 함수
-	//---------------------------------------
-	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
-
-	//CreateSession성공 시 호출 함수
-	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
-	//---------------------------------------
-
-
-	//Session참가 성공시 Delegate 함수
-	//---------------------------------------
-	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
-
-	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
-	//---------------------------------------
-
-	//Session찾기 성공시 Delegate 함수
-	//---------------------------------------
-	void OnFindSessionComplete(bool bWasSuccessful);
-
-	FOnFindSessionsCompleteDelegate FindSessionCompleteDelegate;
-	//---------------------------------------
-
-	IOnlineSessionPtr OnlineSessionInterface;
-
-private:
-	//SessionList
-	TSharedPtr<FOnlineSessionSearch> SessionSearch; 
 };
