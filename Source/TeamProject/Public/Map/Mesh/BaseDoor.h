977421e4279e655debfaa67d89061b39c51ec002
pucat <rosaa9979@gmail.com>
2025-06-09 17:59:24 +0900

    Base Door에 UFUNCTION 추가

--- Source/TeamProject/Public/Map/Mesh/BaseDoor.h
+++ Source/TeamProject/Public/Map/Mesh/BaseDoor.h
@@ -31,6 +31,7 @@ protected:
 
 	UFUNCTION()
 	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	UFUNCTION()
 	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 	
 };
