617292f735327cc5073a3981fcba5d4ab455f3df
이동희 <ldh940627@gmail.com>
2025-06-12 17:04:23 +0900

    캐릭터 무빙 추가

--- Source/TeamProject/Private/Player/Agent.cpp
+++ Source/TeamProject/Private/Player/Agent.cpp
@@ -3,29 +3,48 @@
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "InputMappingContext.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 AAgent::AAgent()
 {
-	PrimaryActorTick.bCanEverTick = true;
-
-	//---------------------------------------------------------------------------
-	FirstViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FIRSTVIEWCAMERA"));
-	//---------------------------------------------------------------------------
+	PrimaryActorTick.bCanEverTick = false;
 
 
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 
-	//---------------------------------------------------------------------------
-	FirstViewCamera->SetupAttachment(GetMesh());
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
 
-	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_AGENT(TEXT("/Game/Creative_Characters_FREE/Skeleton_Meshes/SK_Body_010.SK_Body_010"));
+	// 캐릭터 무브먼트 세팅
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+	// 카메라 세팅
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 200.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> SK_AGENT(TEXT("/Game/_GamePlay/Player/SHIROKO/SHIROKO_SkeletaMesh.SHIROKO_SkeletaMesh"));
 	if(SK_AGENT.Succeeded())
 		GetMesh()->SetSkeletalMesh(SK_AGENT.Object);
+	
 
-	// SJS Start
-	GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, 90.0f));	
-	GetMesh()->SetRelativeRotation(FRotator(0.0f, 10.0f, 0.0f));
-	int a = 100;
-	// SJS End
+	GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -90.0f));
+	GetMesh()->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
+	
 	
 	static ConstructorHelpers::FObjectFinder<UInputMappingContext> DEFAULT_CONTEXT(TEXT("/Game/_GamePlay/Player/Input/IMC_Agent.IMC_Agent"));
 	if (DEFAULT_CONTEXT.Succeeded())
@@ -45,6 +64,12 @@ AAgent::AAgent()
 		LookAction = IA_LOOK.Object;
 	}
 
+	static ConstructorHelpers::FObjectFinder<UInputAction> IA_JUMP(TEXT("/Game/_GamePlay/Player/Input/IA_Jump.IA_Jump"));
+	if (IA_JUMP.Succeeded())
+	{
+		JumpAction = IA_JUMP.Object;
+	}
+
 	//---------------------------------------------------------------------------
 }
 
@@ -55,11 +80,6 @@ void AAgent::BeginPlay()
 	InitInput();
 }
 
-void AAgent::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
 void AAgent::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
@@ -67,6 +87,11 @@ void AAgent::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
 	{
+		// jump
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ACharacter::Jump);
+		EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ACharacter::StopJumping);
+
+		
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAgent::Move);
 		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &AAgent::Look);
 	}
@@ -76,17 +101,33 @@ void AAgent::Move(const FInputActionValue& Value)
 {
 	const FVector2D MovementVector = Value.Get<FVector2D>();
 
-	AddMovementInput(GetActorRightVector(), MovementVector.X);
-	AddMovementInput(GetActorForwardVector(), MovementVector.Y);	
+	if (Controller != nullptr)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
 }
 
 void AAgent::Look(const FInputActionValue& Value)
 {
-	const FVector2D LookVector = Value.Get<FVector2D>();
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
 
-	AddControllerYawInput(LookVector.X);
-	AddControllerPitchInput(-LookVector.Y);
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
 }
+
+
 
 void AAgent::InitInput()
 {
